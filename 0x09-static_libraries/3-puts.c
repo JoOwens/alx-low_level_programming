fb861db42b709c1e50e87c9325c1b0e38f78758c
JoOwens <owinpojose@gmail.com>
2023-03-10 01:43:28 -0800

    0-My lib

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _puts - prints charaters
+ * @s: character input
+ *
+ * Return: Nothing
+ */
+
+void _puts(char *s)
+{
+	puts(s);
+}
+

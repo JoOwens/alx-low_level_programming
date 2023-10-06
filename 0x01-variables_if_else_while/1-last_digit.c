7118561e984d8aeabc7c7b2b9969cb05b7694189
JoOwens <owinpojose@gmail.com>
2023-10-06 18:55:04 +0300

    Alphabeyts Both

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,8 +1,3 @@
-/*
- * File: 1-last_digit.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
@@ -13,6 +8,7 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 	int n;

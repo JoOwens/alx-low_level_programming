badd9ad3e5c80a08df24b1265222c3ec7a0104d7
JoOwens <owinpojose@gmail.com>
2023-10-29 22:57:57 +0300

    Static Lib Sript

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		char *l = haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+
+		if (*p == '\0')
+			return (haystack);
+
+		haystack++;
+	}
+
+	return (0);
+}
+

94cdb4ed5d5fab5f3890382392864ccfa3483caf
JoOwens <owinpojose@gmail.com>
2023-10-31 06:57:21 +0300

    Concentrate Args

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * argstostr - Concatenates all the arguments of a program
+ * @ac: The number of arguments
+ * @av: The array of arguments
+ *
+ * Return: Pointer to the new string, or NULL if it fails
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, k = 0, len = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			len++;
+		len++;
+	}
+
+	str = malloc((len + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+	str[k] = '\0';
+
+	return (str);
+}

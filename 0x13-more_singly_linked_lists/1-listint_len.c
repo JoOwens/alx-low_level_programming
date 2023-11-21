f8f0b7a9c28ef8d693edbc2cb9fc5de1ea8e06eb
JoOwens <owinpojose@gmail.com>
2023-11-21 17:52:27 +0300

    Betty Checks

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,13 +9,13 @@
 
 size_t listint_len(const listint_t *h)
 {
-    size_t len = 0;
+	size_t len = 0;
 
-    while (h)
-    {
-        len++;
-        h = h->next;
-    }
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
 
-    return (len);
+	return (len);
 }
